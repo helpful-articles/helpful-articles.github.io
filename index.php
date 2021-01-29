5139a1f0d59f64d33f5af221e8dbe9d484a72cdd
helpful-articles <77534653+helpful-articles@users.noreply.github.com>
2021-01-29 18:38:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&utm_placement={{placement}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}}&num={num}&reg={reg} 
 
 require_once dirname(__FILE__) . '/kclient.php';
-$client = new KClient('https://app-anntrack.ru/', $_GET['v']);
+$client = new KClient('https://cdn05.app-anntrack.ru/', 'vyhyC6MP9QyW1rwK');
 $client->sendAllParams();       // to send all params from page query
 $client->forceRedirectOffer();       // redirect to offer if an offer is chosen
 // $client->param('sub_id_5', '123'); // you can send any params
@@ -11,7 +11,4 @@ $client->forceRedirectOffer();       // redirect to offer if an offer is chosen
 // $client->debug();              // to enable debug mode and show the errors
 // $client->execute();             // request to api, show the output and continue
 $client->executeAndBreak();     // to stop page execution if there is redirect or some output
-
-include 'index.html';
-
 ?>
